0ebd86e0468e9efd5d778729afd2d740298c798b
froshitech <froshitech@gmail.com>
2026-01-18 08:54:10 +0800

    Add employee profile and payment method fragments; update manager home layout and admin navigation menu with new icons and string resources.

--- app/src/main/java/app/hub/user/UserProfileFragment.java
+++ app/src/main/java/app/hub/user/UserProfileFragment.java
@@ -6,7 +6,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
